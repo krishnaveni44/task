d1c33094d7078e312b601c8167d5a5a8d8c66f98
krishnaveni44 <krishnaveni44@gmail.com>
2022-02-13 16:31:24 +0530

    Todo task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,240 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
-function App() {
+
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new obkect
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+// import { useState } from "react";
+// import "./App.css";
+// // import IconButton from '@mui/material/IconButton';
+// // import DeleteIcon from '@mui/icons-material/Delete';
+// // import Button from '@mui/material/Button';
+// // import EditIcon from "@mui/icons-material/Edit";
+
+// import React from "react";
+
+
+// function Todo({ todo, index, completeTodo, removeTodo }) {
+//   return (
+//     <div
+//       className="todo"
+//       style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+//     >
+//       {todo.text}
+//       <div>
+//         <button className="combut" onClick={() => completeTodo(index)}>
+//          Complete
+//         </button>
+//         <button className="delbut" onClick={() => removeTodo(index)}>
+//           Delete 🧨🧨
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function TodoForm({ addTodo }) {
+//   const [value, setValue] = useState("");
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     if (!value) return;
+//     addTodo(value);
+//     setValue("");
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         className="input"
+//         value={value}
+//         placeholder="Add Task...📝"
+//         onChange={e => setValue(e.target.value)}
+//       />
+//     </form>
+//   );
+// }
+
+// function App() {
+//   const [todos, setTodos] = React.useState([
+//     {
+//       text: "Learn about React",
+//       isCompleted: false
+//     },
+//     {
+//       text: "Meet friend for lunch",
+//       isCompleted: false
+//     },
+//     {
+//       text: "Build really cool todo app",
+//       isCompleted: false
+//     }
+//   ]);
+
+//   const addTodo = text => {
+//     const newTodos = [...todos, { text }];
+//     setTodos(newTodos);
+//   };
+//   // const allTodos = allTodo.map((e) => {e.addTodo}
+//   // );
+  
+//   const completeTodo = index => {
+//     const newTodos = [...todos];
+//     newTodos[index].isCompleted = true;
+//     setTodos(newTodos);
+//   };
+
+//   const removeTodo = index => {
+//     const newTodos = [...todos];
+//     newTodos.splice(index, 1);
+//     setTodos(newTodos);
+//   };
+
+//   return (
+//     <div className="app">
+//       <div className="menuCont">
+//         <button className="act">All</button>
+//         <button>Active</button>
+//         <button>Completed</button>
+//       </div>
+//       <div className="todo-list">
+//         {todos.map((todo, index) => (
+//           <Todo
+//             key={index}
+//             index={index}
+//             todo={todo}
+//             completeTodo={completeTodo}
+//             removeTodo={removeTodo}
+//           />
+//         ))}
+//         <TodoForm addTodo={addTodo} />
+//       </div>
+   
+//     </div>
+//   );
+// }
+
+// export default App;
